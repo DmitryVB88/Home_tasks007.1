6fdf50a4bb10632faceaf40930386ffed4ef797d
Dima <dima.brashovyan@mail.ru>
2022-06-05 22:45:51 +0300

    отсортировали строки массива от большего к меньшему

---
+++ Second_task_sort_array_elements_in_descending_order/Program.cs
@@ -0,0 +1,43 @@
+﻿// 2.Написать программу, упорядочивания по убыванию элементы каждой строки двумерного массива.
+
+Console.Clear();
+Console.WriteLine();
+int[,] matrix = new int [3,4];
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(1, 10);
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    int tmp = 0;
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for(int k = 0; k < matrix.GetLength(1) - 1 - j; k++)
+        {
+            if(matrix[i,k] < matrix[i, k + 1])
+            {
+               tmp = matrix[i, k];
+               matrix[i, k] = matrix[i, k + 1];
+               matrix[i, k + 1] = tmp;
+            }
+        }
+     
+    } 
+     Console.WriteLine();  
+} 
+
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
