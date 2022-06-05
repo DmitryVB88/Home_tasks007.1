618a514a859ededd32d364098567b16bf2fa0ad8
Dima <dima.brashovyan@mail.ru>
2022-06-05 22:42:59 +0300

    Поменяли местами заполнение строк в массиве

---
+++ First_task_change_lines/Program.cs
@@ -0,0 +1,34 @@
+﻿// 1. Написать программу, которая обменивает элементы первой строки и последней строки.
+
+int[,] matrix = new int[2, 4];
+for(int row = 0; row < matrix.GetLength(0); row++)
+  {
+       for(int column = 0; column < matrix.GetLength(1); column++)
+       {
+       matrix[row, column] = new Random().Next(2, 10);
+       }
+    
+  }  
+
+for(int row = 0; row < matrix.GetLength(0); row++)
+  {
+       for(int column = 0; column < matrix.GetLength(1); column++)
+       {
+    Console.Write($"{matrix[row, column]} ");
+    }
+Console.WriteLine();
+}
+Console.WriteLine();
+
+
+int[,] newMatrix = new int [2,4];
+for(int i = 1; i < newMatrix.GetLength(0); i--)
+{
+      for(int j = 0; j < newMatrix.GetLength(1); j++)
+      {
+         
+        newMatrix[i, j] = matrix[i, j];
+        Console.Write($"{newMatrix[i, j]} ");
+      }
+    Console.WriteLine();
+}
